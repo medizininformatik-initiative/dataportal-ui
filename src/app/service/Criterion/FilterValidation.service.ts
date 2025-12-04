43add1dc3024cc1bb7cde70770297d0f8168a9a2
shah1375 <shayanmohajerani@gmail.com>
2025-12-04 13:21:23 +0100

    Refactor CriterionMetadataService and FilterValidationService for improved clarity and functionality

--- src/app/service/Criterion/FilterValidation.service.ts
+++ src/app/service/Criterion/FilterValidation.service.ts
@@ -3,7 +3,6 @@ import { AttributeFilter } from 'src/app/model/FeasibilityQuery/Criterion/Attrib
 import { FilterTypesService } from '../FilterTypes.service';
 import { Injectable } from '@angular/core';
 import { ValueFilter } from 'src/app/model/FeasibilityQuery/Criterion/AttributeFilter/ValueFilter';
-import { ConceptFilter } from 'src/app/model/FeasibilityQuery/Criterion/AttributeFilter/Concept/ConceptFilter';
 
 @Injectable({
   providedIn: 'root',
@@ -11,12 +10,24 @@ import { ConceptFilter } from 'src/app/model/FeasibilityQuery/Criterion/Attribut
 export class FilterValidationService {
   constructor(private filterTypeService: FilterTypesService) {}
 
-  public isConceptFilterSet(conceptFilter: ConceptFilter, optional: boolean): boolean {
-    return optional ? true : this.hasSelectedConcepts(conceptFilter);
+  public hasAllRequiredValueFilters(valueFilters: ValueFilter[]): boolean {
+    return this.requiredFiltersAreSet(valueFilters);
   }
 
-  private hasSelectedConcepts(conceptFilter: ConceptFilter): boolean {
-    return conceptFilter.getSelectedConcepts().length > 0;
+  public hasAllRequiredAttributeFilters(attributeFilters: AttributeFilter[]): boolean {
+    return this.requiredFiltersAreSet(attributeFilters);
+  }
+
+  private requiredFiltersAreSet(filters: AbstractAttributeFilters[]): boolean {
+    return filters.some((filter) => !filter.getOptional() || this.isFilterComplete(filter));
+  }
+
+  private isFilterComplete(filter: AbstractAttributeFilters): boolean {
+    return this.hasSelectedConcepts(filter) || this.hasSetQuantity(filter);
+  }
+
+  private hasSelectedConcepts(filter: AbstractAttributeFilters): boolean {
+    return filter.getConcept()?.getSelectedConcepts().length > 0;
   }
 
   private hasSetQuantity(filter: AbstractAttributeFilters): boolean {
