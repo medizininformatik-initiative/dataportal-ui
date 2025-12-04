86906d003e5a8214964d5cc7fab9a6990af1a872
shah1375 <shayanmohajerani@gmail.com>
2025-12-04 10:25:51 +0100

    Refactor action bars to remove upload and download functionalities, streamline button orders, and enhance UI consistency

--- src/app/modules/feasibility-query/components/editor/action-bar/editor-action-bar.component.ts
+++ src/app/modules/feasibility-query/components/editor/action-bar/editor-action-bar.component.ts
@@ -3,7 +3,6 @@ import { DataQueryValidationService } from 'src/app/service/DataQuery/DataQueryV
 import { FeasibilityQueryValidation } from 'src/app/service/Criterion/FeasibilityQueryValidation.service';
 import { NavigationHelperService } from 'src/app/service/NavigationHelper.service';
 import { Observable, of, Subscription } from 'rxjs';
-import { SaveDataQueryModalService } from 'src/app/service/SaveDataQueryModal.service';
 import { StageProviderService } from '../../../../../service/Provider/StageProvider.service';
 import { ValidDataQuery } from 'src/app/model/Types/ValidDataQuery';
 
@@ -24,25 +23,16 @@ export class EditorActionBarComponent implements OnInit, OnDestroy {
     private dataQueryValidation: DataQueryValidationService,
     private feasibilityQueryValidation: FeasibilityQueryValidation,
     private stageProviderService: StageProviderService,
-    private navigationHelperService: NavigationHelperService,
-    private saveDataQueryModalService: SaveDataQueryModalService
+    private navigationHelperService: NavigationHelperService
   ) {}
 
   ngOnInit() {
     this.isFeasibilityQueryValid$ = this.feasibilityQueryValidation.getIsFeasibilityQueryValid();
-    this.stageArray$ = this.stageProviderService.getStageUIDArray();
     this.validDataQuery$ = this.dataQueryValidation.validateDataQuery();
   }
 
   ngOnDestroy() {
     this.saveDataQueryModalSubscription?.unsubscribe();
-  }
-
-  public saveDataQuery() {
-    this.saveDataQueryModalSubscription?.unsubscribe();
-    this.saveDataQueryModalSubscription = this.saveDataQueryModalService
-      .openSaveDataQueryModal()
-      .subscribe();
   }
 
   public navigateToSearch() {
@@ -51,10 +41,6 @@ export class EditorActionBarComponent implements OnInit, OnDestroy {
 
   public doSendRequest(): void {
     this.navigationHelperService.navigateToFeasibilityQueryResult();
-  }
-
-  public navigateToDataRequestCohortDefinition(): void {
-    this.navigationHelperService.navigateToDataQueryCohortDefinition();
   }
 
   public navigateToBulkCriteriaSearch(): void {
