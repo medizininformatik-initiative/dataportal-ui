3cd880167d2cd999864ebb6f2fdc3760fc723da7
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2025-12-10 15:41:37 +0100

    cleanup css and non-used component

--- src/app/modules/query-editor/query-editor.module.ts
+++ src/app/modules/query-editor/query-editor.module.ts
@@ -11,7 +11,6 @@ import { MaterialModule } from 'src/app/layout/material/material.module';
 import { NgModule } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { ProfileComponent } from './components/editor-content/profile/profile.component';
-import { ProfileFilterComponent } from './components/editor-content/profile/profile-filter/profile-filter.component';
 import { ProfileHeaderComponent } from './components/editor-content/profile/header/profile-header.component';
 import { QueryEditorComponent } from './components/query-editor.component';
 import { QueryEditorRoutingModule } from './query-editor.routing.module';
@@ -65,7 +64,6 @@ export const FORMATS_GERMAN = {
     ProfileHeaderComponent,
     QueryEditorComponent,
     TokenFilterComponent,
-    ProfileFilterComponent,
     ProfileTimeFilterComponent,
     ProfileReferenceComponent,
     ProfileReferenceModalComponent,
