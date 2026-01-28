481f5c2c85a7da57d71c4f127e90112fda762371
shah1375 <shayanmohajerani@gmail.com>
2026-01-28 09:39:56 +0100

    refactor SavedQueryTileComponent to improve error handling and validation logic

--- src/app/shared/components/saved-query-tile/saved-query-tile.component.ts
+++ src/app/shared/components/saved-query-tile/saved-query-tile.component.ts
@@ -1,13 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { InterfaceSavedQueryTile } from '../../models/SavedQueryTile/InterfaceSavedQueryTile';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ConfirmDeleteService } from '../../service/SavedQueryTile/ConfirmDelete.service';
+import { InterfaceSavedQueryTile } from '../../models/SavedQueryTile/InterfaceSavedQueryTile';
 
 @Component({
   selector: 'num-saved-query-tile',
   templateUrl: './saved-query-tile.component.html',
   styleUrls: ['./saved-query-tile.component.scss'],
 })
-export class SavedQueryTileComponent {
+export class SavedQueryTileComponent implements OnInit {
   @Input()
   savedQuery: InterfaceSavedQueryTile;
 
@@ -17,10 +17,31 @@ export class SavedQueryTileComponent {
   @Output()
   navigate = new EventEmitter<string>();
 
+  @Output()
+  loadValidationReport = new EventEmitter<string>();
+
+  crtdlIsValid: boolean;
+
   constructor(private confirmDeleteService: ConfirmDeleteService) {}
+
+  ngOnInit() {
+    this.isValidAndExists();
+  }
 
   public onNavigate(id: string) {
     this.navigate.emit(id);
+  }
+
+  public onLoadValidationReport(id: string) {
+    this.loadValidationReport.emit(id);
+  }
+
+  public isValidAndExists() {
+    this.crtdlIsValid =
+      this.savedQuery.ccdl.exists &&
+      this.savedQuery.ccdl.isValid &&
+      this.savedQuery.dataExtraction.exists &&
+      this.savedQuery.dataExtraction.isValid;
   }
 
   public onDelete(id: string) {
