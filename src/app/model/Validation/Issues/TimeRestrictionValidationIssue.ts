2e93adfaf9441ea8447fe1be908dd15317785e46
shah1375 <shayanmohajerani@gmail.com>
2026-01-28 14:35:31 +0100

    add TimeRestrictionValidationIssue class with JSON deserialization method

--- src/app/model/Validation/Issues/TimeRestrictionValidationIssue.ts
+++ src/app/model/Validation/Issues/TimeRestrictionValidationIssue.ts
@@ -1,3 +1,5 @@
+import { TimeRestrictionValidationIssueData } from 'src/app/core/model/Validation/ValidationIssueData';
+
 export class TimeRestrictionValidationIssue {
   private readonly afterDate: string;
   private readonly beforeDate: string;
@@ -13,5 +15,11 @@ export class TimeRestrictionValidationIssue {
 
   public getBeforeDate(): string | undefined {
     return this.beforeDate;
+  }
+
+  public static fromJson(data: TimeRestrictionValidationIssueData): TimeRestrictionValidationIssue {
+    const afterDate = data.timeRestriction.afterDate;
+    const beforeDate = data.timeRestriction.beforeDate;
+    return new TimeRestrictionValidationIssue(afterDate, beforeDate);
   }
 }
