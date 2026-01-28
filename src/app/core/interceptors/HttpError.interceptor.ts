27dda03564a117e08fcbb0edb43daf59d797c8f2
shah1375 <shayanmohajerani@gmail.com>
2026-01-28 09:29:26 +0100

    clean up

--- src/app/core/interceptors/HttpError.interceptor.ts
+++ src/app/core/interceptors/HttpError.interceptor.ts
@@ -15,13 +15,14 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   constructor(private errorHandler: HttpErrorHandlerService) {}
 
   /**
+   * Intercepts HTTP responses to handle errors globally.
    * @param req
    * @param next
    * @returns
    */
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(
-      catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error, req.url))
-    );
+    return next
+      .handle(req)
+      .pipe(catchError((error: HttpErrorResponse) => this.errorHandler.handleError(error, req.url)));
   }
 }
