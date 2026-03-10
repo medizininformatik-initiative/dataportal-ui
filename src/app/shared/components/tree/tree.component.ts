f09dd25f44e45f8d7fea0109410222a5cc0f48fd
shah1375 <shayanmohajerani@gmail.com>
2026-03-10 16:44:18 +0100

    refactor: adjust margin calculation for tree node checkbox for improved layout

--- src/app/shared/components/tree/tree.component.ts
+++ src/app/shared/components/tree/tree.component.ts
@@ -44,7 +44,7 @@ export class TreeComponent implements OnInit {
       if (level > 1) {
         return `calc(${level} * 35px)`;
       } else {
-        return `calc(${level} * 30px)`;
+        return `calc(${level} * 38px)`;
       }
     }
   }
