35faf3b6527eb41c7298b0df554ce894b78ab5e0
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2026-03-11 15:45:00 +0100

    update new provider calls

--- src/app/service/Translator/DataExtraction/ProfileFilterTranslator.service.ts
+++ src/app/service/Translator/DataExtraction/ProfileFilterTranslator.service.ts
@@ -1,14 +1,6 @@
 import { AbstractProfileFilter } from 'src/app/model/DataSelection/Profile/Filter/AbstractProfileFilter';
 import { AttributeGroupsData } from 'src/app/model/Interface/AttributeGroupsData';
 import { DataSelectionProfile } from 'src/app/model/DataSelection/Profile/DataSelectionProfile';
-import { Display } from 'src/app/model/DataSelection/Profile/Display';
-import { Injectable } from '@angular/core';
-import { ProfileTimeRestrictionFilter } from 'src/app/model/DataSelection/Profile/Filter/ProfileDateFilter';
-import { ProfileTokenFilter } from 'src/app/model/DataSelection/Profile/Filter/ProfileTokenFilter';
-import { TerminologyCode } from 'src/app/model/Terminology/TerminologyCode';
-import { TypeGuard } from '../../TypeGuard/TypeGuard';
-import { UITimeRestrictionFactoryService } from '../Shared/UITimeRestrictionFactory.service';
-import { v4 as uuidv4 } from 'uuid';
 import { Injectable } from '@angular/core';
 import { ProfileDateFilterTranslatorService } from './ProfileDateFilterTranslator.service';
 import { ProfileTokenFilterTranslatorService } from './ProfileTokenFilterTranslator.service';
