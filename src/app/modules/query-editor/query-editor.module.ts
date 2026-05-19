88867428ddf25fd1b31fcccff8f14ed9d953d6f9
shah1375 <shayanmohajerani@gmail.com>
2026-05-19 12:23:07 +0200

    removed @angular/flex-layout usage in html and replaced with css classes.
    clean up up of css
    
    added global query editor header theme

--- src/app/modules/query-editor/query-editor.module.ts
+++ src/app/modules/query-editor/query-editor.module.ts
@@ -1,35 +1,34 @@
-import { CommonModule } from '@angular/common';
-import { CriterionComponent } from './components/editor-content/criterion/criterion.component';
-import { CriterionHeaderComponent } from './components/editor-content/criterion/header/criterion-header.component';
-import { DataSelectionModule } from '../data-selection/data-selection.module';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { EditActionBarComponent } from './components/action-bar/edit-action-bar.component';
-import { EditorContentComponent } from './components/editor-content/editor-content.component';
-import { EditorHeaderComponent } from './components/editor-header/editor-header.component';
-import { FeasibilityQueryModule } from '../feasibility-query/feasibility-query.module';
-import { FilterTabsComponent } from './components/editor-content/filter-tabs/filter-tabs.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MaterialModule } from 'src/app/layout/material/material.module';
-import { NgModule } from '@angular/core';
-import { OverlayModule } from '@angular/cdk/overlay';
-import { PossibleReferencesComponent } from './components/editor-content/profile/reference/possible-references/possible-references.component';
-import { ProfileComponent } from './components/editor-content/profile/profile.component';
-import { ProfileHeaderComponent } from './components/editor-content/profile/header/profile-header.component';
-import { ProfileReferenceComponent } from './components/editor-content/profile/reference/profile-reference.component';
-import { ProfileReferenceModalComponent } from './components/editor-content/profile/reference/modal-window/profile-reference-modal.component';
-import { ProfileTimeFilterComponent } from './components/editor-content/profile/profile-filter/profile-time-restriction/profile-time-filter.component';
-import { QueryEditorComponent } from './components/query-editor.component';
-import { QueryEditorRoutingModule } from './query-editor.routing.module';
-import { ReferenceEditComponent } from './components/editor-content/reference/reference-edit.component';
-import { ReferenceFieldTabComponent } from './components/editor-content/profile/reference/reference-field-tab/reference-field-tab.component';
-import { SharedFilterModule } from '../shared-filter/shared-filter.module';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { TokenFilterComponent } from './components/editor-content/profile/profile-filter/token-filter/token-filter.component';
+import { CommonModule } from '@angular/common'
+import { CriterionComponent } from './components/editor-content/criterion/criterion.component'
+import { CriterionHeaderComponent } from './components/editor-content/criterion/header/criterion-header.component'
+import { DataSelectionModule } from '../data-selection/data-selection.module'
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core'
+import { EditActionBarComponent } from './components/action-bar/edit-action-bar.component'
+import { EditorContentComponent } from './components/editor-content/editor-content.component'
+import { FeasibilityQueryModule } from '../feasibility-query/feasibility-query.module'
+import { FilterTabsComponent } from './components/editor-content/filter-tabs/filter-tabs.component'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { MaterialModule } from 'src/app/layout/material/material.module'
+import { NgModule } from '@angular/core'
+import { OverlayModule } from '@angular/cdk/overlay'
+import { PossibleReferencesComponent } from './components/editor-content/profile/reference/possible-references/possible-references.component'
+import { ProfileComponent } from './components/editor-content/profile/profile.component'
+import { ProfileHeaderComponent } from './components/editor-content/profile/header/profile-header.component'
+import { ProfileReferenceComponent } from './components/editor-content/profile/reference/profile-reference.component'
+import { ProfileReferenceModalComponent } from './components/editor-content/profile/reference/modal-window/profile-reference-modal.component'
+import { ProfileTimeFilterComponent } from './components/editor-content/profile/profile-filter/profile-time-restriction/profile-time-filter.component'
+import { QueryEditorComponent } from './components/query-editor.component'
+import { QueryEditorRoutingModule } from './query-editor.routing.module'
+import { ReferenceEditComponent } from './components/editor-content/reference/reference-edit.component'
+import { ReferenceFieldTabComponent } from './components/editor-content/profile/reference/reference-field-tab/reference-field-tab.component'
+import { SharedFilterModule } from '../shared-filter/shared-filter.module'
+import { SharedModule } from 'src/app/shared/shared.module'
+import { TokenFilterComponent } from './components/editor-content/profile/profile-filter/token-filter/token-filter.component'
 import {
   MAT_MOMENT_DATE_ADAPTER_OPTIONS,
   MomentDateAdapter,
-} from '@angular/material-moment-adapter';
+} from '@angular/material-moment-adapter'
 
 export const FORMATS_GERMAN = {
   parse: {
@@ -41,7 +40,7 @@ export const FORMATS_GERMAN = {
     dateA11yLabel: 'LL',
     monthYearA11yLabel: 'YYYY',
   },
-};
+}
 
 @NgModule({
   imports: [
@@ -57,7 +56,6 @@ export const FORMATS_GERMAN = {
     FontAwesomeModule,
   ],
   declarations: [
-    EditorHeaderComponent,
     EditActionBarComponent,
     EditorContentComponent,
     CriterionComponent,
