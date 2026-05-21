a0907bbe10a7813827500124087251cbca3f6743
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2026-05-21 14:21:09 +0200

    fix condition for criterion header chips

--- src/app/shared/service/FilterChips/DataSelection/DataSelectionFiltersFilterChips.service.ts
+++ src/app/shared/service/FilterChips/DataSelection/DataSelectionFiltersFilterChips.service.ts
@@ -1,11 +1,11 @@
-import { AbstractProfileFilter } from 'src/app/model/DataSelection/Profile/Filter/AbstractProfileFilter';
-import { DataSelectionUIType } from 'src/app/model/Utilities/DataSelectionUIType';
-import { FilterChipConceptAdapter } from 'src/app/shared/models/FilterChips/Adapter/FilterChipConceptAdapter';
-import { FilterChipData } from 'src/app/shared/models/FilterChips/FilterChipData';
-import { Injectable } from '@angular/core';
-import { ProfileTimeRestrictionFilter } from 'src/app/model/DataSelection/Profile/Filter/ProfileDateFilter';
-import { ProfileTokenFilter } from 'src/app/model/DataSelection/Profile/Filter/ProfileTokenFilter';
-import { TimeRestrictionChipService } from '../Criterion/TimeRestrictionChip.service';
+import { AbstractProfileFilter } from 'src/app/model/DataSelection/Profile/Filter/AbstractProfileFilter'
+import { DataSelectionUIType } from 'src/app/model/Utilities/DataSelectionUIType'
+import { FilterChipConceptAdapter } from 'src/app/shared/models/FilterChips/Adapter/FilterChipConceptAdapter'
+import { FilterChipData } from 'src/app/shared/models/FilterChips/FilterChipData'
+import { Injectable } from '@angular/core'
+import { ProfileTimeRestrictionFilter } from 'src/app/model/DataSelection/Profile/Filter/ProfileDateFilter'
+import { ProfileTokenFilter } from 'src/app/model/DataSelection/Profile/Filter/ProfileTokenFilter'
+import { TimeRestrictionChipService } from '../Criterion/TimeRestrictionChip.service'
 
 @Injectable({
   providedIn: 'root',
@@ -17,32 +17,32 @@ export class DataSelectionFiltersFilterChips {
     filters: AbstractProfileFilter[]
   ): FilterChipData[] {
     return filters.reduce<FilterChipData[]>((filterChips, filter) => {
-      filterChips.push(...this.getChipsForFilter(filter));
-      return filterChips;
-    }, []);
+      filterChips.push(...this.getChipsForFilter(filter))
+      return filterChips
+    }, [])
   }
 
   private getChipsForFilter(filter: AbstractProfileFilter): FilterChipData[] {
     switch (filter.getUiType()) {
       case DataSelectionUIType.TIMERESTRICTION:
-        return this.getTimeRestrictionChips(filter as ProfileTimeRestrictionFilter);
+        return this.getTimeRestrictionChips(filter as ProfileTimeRestrictionFilter)
       case DataSelectionUIType.CODE:
-        return this.getCodeFilterChips(filter as ProfileTokenFilter);
+        return this.getCodeFilterChips(filter as ProfileTokenFilter)
       default:
-        return [];
+        return []
     }
   }
 
   private getTimeRestrictionChips(filter: ProfileTimeRestrictionFilter): FilterChipData[] {
     return this.timeRestrictionFilterChipsService.generateTimeRestrictionChips(
       filter.getTimeRestriction()
-    );
+    )
   }
 
   private getCodeFilterChips(filter: ProfileTokenFilter): FilterChipData[] {
     return FilterChipConceptAdapter.adaptCodeableConcept(
       filter.getSelectedTokens(),
-      'EDITOR.CONTENT.PROFILE.TAB_LABEL.' + filter.getType().toUpperCase()
-    );
+      'EDITOR.CONTENT.TAB_LABEL.' + filter.getType().toUpperCase()
+    )
   }
 }
