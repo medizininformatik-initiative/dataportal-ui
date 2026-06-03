80d8f18535373a134110f69215437c15132feabe
shah1375 <shayanmohajerani@gmail.com>
2026-06-03 12:54:19 +0200

    feat: enhance dashboard layout and add FAQ translations

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,36 +1,37 @@
-import { Component, OnInit, inject } from '@angular/core'
-import { NavigationHelperService } from 'src/app/service/NavigationHelper.service'
-import { OAuthService } from 'angular-oauth2-oidc'
-import { TranslateModule, TranslateService } from '@ngx-translate/core'
+import { AppSettingsProviderService } from 'src/app/service/Config/AppSettingsProvider.service'
 import { ButtonComponent } from 'src/app/shared/components/button/button.component'
+import { Component, inject, OnInit } from '@angular/core'
+import { DashboardFaqComponent } from '../dashboard-faq/dashboard-faq.component'
 import { HeaderComponent } from 'src/app/shared/components/header/header.component'
 import { HeaderDescriptionComponent } from 'src/app/shared/components/header-description/header-description.component'
+import { NavigationHelperService } from 'src/app/service/NavigationHelper.service'
+import { TranslateModule } from '@ngx-translate/core'
 
-/**
- * @todo Needs to be refactored
- * User directive possibly not needed
- */
 @Component({
   selector: 'num-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
-  imports: [TranslateModule, HeaderComponent, HeaderDescriptionComponent, ButtonComponent],
+  imports: [
+    TranslateModule,
+    HeaderComponent,
+    HeaderDescriptionComponent,
+    ButtonComponent,
+    DashboardFaqComponent,
+  ],
 })
 export class DashboardComponent implements OnInit {
-  private oauthService = inject(OAuthService)
-  translate = inject(TranslateService)
+  private appSettingsProviderService = inject(AppSettingsProviderService)
   private navigationHelperService = inject(NavigationHelperService)
 
-  /** Inserted by Angular inject() migration for backwards compatibility */
-  constructor(...args: unknown[])
+  ngOnInit(): void {}
 
   constructor() {}
 
-  displayInfoMessage = false
-  proposalPortalLink = ''
-
-  ngOnInit(): void {}
+  public navigateToProposalPortal(): void {
+    const link = this.appSettingsProviderService.getPortalLink()
+    window.open(link, '_blank')
+  }
 
   public navigateToDataQueryEditor() {
     this.navigationHelperService.navigateToDataQueryCohortDefinition()
@@ -46,10 +47,6 @@ export class DashboardComponent implements OnInit {
 
   public navigateToSavedQueries() {
     this.navigationHelperService.navigateToSavedQueries()
-  }
-
-  public openProposalPortalLink(): void {
-    window.open(this.proposalPortalLink, '_blank', 'noopener')
   }
 
   public navigateToFeasibilityEditor() {
