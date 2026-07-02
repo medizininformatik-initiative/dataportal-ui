e617aebf4149c936a468c7c3e502ddc9198c2791
shah1375 <shayanmohajerani@gmail.com>
2026-07-02 13:49:18 +0200

    feat: enhance validation logic in FeasibilityQueryValidationService to incorporate criteria from the criterion provider

--- src/app/service/Criterion/Validation/FeasibilityQueryValidationService.service.ts
+++ src/app/service/Criterion/Validation/FeasibilityQueryValidationService.service.ts
@@ -22,16 +22,23 @@ export class FeasibilityQueryValidationService {
   private readonly criterionService = inject(CriterionProviderService)
   private readonly filterTypeService = inject(FilterTypesService)
   private readonly feasibilityQueryService = inject(FeasibilityQueryProviderService)
+  private criterionProvider = inject(CriterionProviderService)
 
   private readonly activeFeasibilityQuery = toSignal<FeasibilityQuery | null>(
     this.feasibilityQueryService.getActiveFeasibilityQuery().pipe(map((query) => query ?? null)),
     { initialValue: null }
   )
 
+  readonly criteria = toSignal(this.criterionProvider.getAll(), {
+    initialValue: [],
+  })
+
   private readonly validationState = computed<ValidationState>(() => {
     const feasibilityQuery = this.activeFeasibilityQuery()
+    const criteria = this.criteria()
+
     if (!feasibilityQuery) return INITIAL_STATE
-    return this.buildValidationState(feasibilityQuery)
+    return this.buildValidationState(feasibilityQuery, criteria)
   })
 
   public readonly isFeasibilityQueryValid = computed<boolean>(() =>
@@ -43,14 +50,19 @@ export class FeasibilityQueryValidationService {
    * @param {FeasibilityQuery} feasibilityQuery
    * @returns {ValidationState}
    */
-  private buildValidationState(feasibilityQuery: FeasibilityQuery): ValidationState {
+  private buildValidationState(
+    feasibilityQuery: FeasibilityQuery,
+    criteria: Criterion[]
+  ): ValidationState {
+    const criteriaById = new Map(criteria.map((c) => [c.getId(), c]))
+
     const allCriteria = this.flattenCriteria(feasibilityQuery)
-    const getCriterion = (id: string) => this.criterionService.getOne(id)
 
     return {
-      criterionIdsWithMissingFilters: allCriteria.filter((id) =>
-        this.isFilterRequired(getCriterion(id))
-      ),
+      criterionIdsWithMissingFilters: allCriteria.filter((id) => {
+        const criterion = criteriaById.get(id)
+        return criterion ? this.isFilterRequired(criterion) : true
+      }),
       hasInclusionCriteria: feasibilityQuery.getInclusionCriteria().length > 0,
     }
   }
