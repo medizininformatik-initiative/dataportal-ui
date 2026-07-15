026106e90bc1715aa2f61e9399b401585cb17622
shah1375 <shayanmohajerani@gmail.com>
2026-07-15 13:13:17 +0200

    clean up

--- src/app/shared/models/TableData/cells/CheckboxCellData.ts
+++ src/app/shared/models/TableData/cells/CheckboxCellData.ts
@@ -1,8 +1,5 @@
-import { Display } from 'src/app/model/DataSelection/Profile/Display';
-
 export interface CheckboxCellData {
   type: 'checkbox'
-  value: string | Display
   isSelected: boolean
   isDisabled: boolean
 }
