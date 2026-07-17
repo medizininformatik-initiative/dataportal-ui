e00c5fce31276973d4a9eb350a16012ec825cef0
shah1375 <shayanmohajerani@gmail.com>
2026-07-17 21:08:18 +0200

    feat: add new profile entry endpoints to DataSelectionPaths

--- src/app/service/Backend/Paths/DataSelectionPaths.ts
+++ src/app/service/Backend/Paths/DataSelectionPaths.ts
@@ -3,7 +3,15 @@ export class DataSelectionPaths {
   public static readonly PROFILE_DATA_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-data?ids=`
   public static readonly PROFILE_TREE_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-tree`
   public static readonly PROFILE_SEARCH_ENTRY_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-tree-entry`
-  public static getBaseUrl() {
+  public static readonly PROFILE_SEARCH_ENDPOINT = `feature/entry/search`
+  public static readonly PROFILE_ENTRY_DETAILS_ENDPOINT = `feature/entry`
+  public static readonly PROFILE_ENTRY_DETAILS_LIST_ENDPOINT = `list-details`
+
+  /**
+   * Returns the base url
+   * @returns {string}
+   */
+  public static getBaseUrl(): string {
     return this.BASE_URL
   }
 }
