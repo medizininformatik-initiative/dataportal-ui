5d665c2ba05f8e490faca43c48f4efeb8110bafd
shah1375 <shayanmohajerani@gmail.com>
2026-07-21 14:28:01 +0200

    updated paths

--- src/app/service/Backend/Paths/DataSelectionPaths.ts
+++ src/app/service/Backend/Paths/DataSelectionPaths.ts
@@ -1,12 +1,10 @@
 export class DataSelectionPaths {
   private static readonly BASE_URL = 'dse'
   public static readonly PROFILE_DATA_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-data?ids=`
-  public static readonly PROFILE_TREE_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-tree`
-  public static readonly PROFILE_SEARCH_ENTRY_ENDPOINT = `${DataSelectionPaths.BASE_URL}/profile-tree-entry`
-  public static readonly PROFILE_SEARCH_ENDPOINT = `feature/entry/search`
-  public static readonly PROFILE_ENTRY_DETAILS_ENDPOINT = `feature/entry`
+  public static readonly PROFILE_SEARCH_ENDPOINT = `profile/entry/search`
+  public static readonly PROFILE_ENTRY_DETAILS_ENDPOINT = `profile/entry`
   public static readonly PROFILE_ENTRY_DETAILS_LIST_ENDPOINT = `list-details`
-
+  public static readonly PROFILE_SEARCH_FILTER = `profile/search/filter`
   /**
    * Returns the base url
    * @returns {string}
